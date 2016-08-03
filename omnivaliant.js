0abad1019ef1ee78eccd0d09770a3462b186579e
Mohit Patel <omnivalient@gmail.com>
2016-08-02 23:30:19 -0400

    Organized the code into a CSS and JS file, and added some background animation to the website.

---
+++ omnivaliant.js
@@ -0,0 +1,24 @@
+﻿(function () {
+
+    var c = document.getElementById('bubbles'),
+        randomN = function (start, end) {
+            return Math.random() * end + start;
+        },
+        i = 0,
+        generateBubble = function () {
+            if (i < 20) {
+                var el = document.createElement('div'),
+                    size = randomN(5, 10);
+                el.setAttribute('style', 'width: ' + size + 'px; height: ' + size + 'px; left:' + randomN(1, c.offsetWidth - (size + 4)) + 'px;');
+                c.appendChild(el);
+                i++;
+            } else {
+                clearInterval(inter);
+            }
+        };
+
+    generateBubble();
+
+    var inter = setInterval(generateBubble, 500);
+
+})();
