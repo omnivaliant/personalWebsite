52439e69b179991329489e0cc25352bb3c0adb84
Mohit Patel <omnivalient@gmail.com>
2016-08-16 15:38:46 -0400

    Removed most elements on the page. Added a moving square with a dragabble paragraph.

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("Text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("Text");
+    document.getElementById(data).innerHTML = "Thank you! :)"
+    document.getElementById(data).style.color = "aquamarine";
+    ev.target.appendChild(document.getElementById(data));
+}
