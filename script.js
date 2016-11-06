22bab16bc91077a265c2527df455ab4cb8084e1c
Mohit Patel <omnivaliant@gmail.com>
2016-11-05 21:55:38 -0400

    Redesigned the webpage, adding more sections and background images.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 window.onload = function start(){
     
     setTimeout(function (){
-        $("p.name").animate({top:'-20px'}, "slow")
+        $("p.name").animate({height:'auto',top:'-10px'}, "slow")
     },1200);
     setTimeout(function (){
-        $("p.intro-text").animate({top:'-50px'}, "slow")
+        $(".description").animate({height:'auto',top:'-50px'}, "slow")
     },1200);
     setTimeout(function () {
         $("img.img-circle").animate({height:'auto',width: '10%'}, "slow")
     },1200);
     setTimeout(function () {
-        $("a.icon-label").fadeIn(1200);
+        $(".social").fadeIn(1200);
     },2000);
 }
