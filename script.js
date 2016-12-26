dcd4e7fc90bcf84c5e6291731e03fa934ac11658
Mohit Patel <omnivaliant@gmail.com>
2016-12-26 00:25:13 -0500

    Changed backgrounds and animations.

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-window.onload = function start(){
+$(function() {
+
+    $(".fa-linkedin-square").mouseover(function(){
+        $(this).css("color","red");
+    });
+    $(".fa-github").mouseover(function(){
+        $(this).css("color","red");
+    });
     
-    setTimeout(function (){
-        $("p.name").animate({height:'auto',top:'-10px'}, "slow")
-    },1200);
-    setTimeout(function (){
-        $(".description").animate({height:'auto',top:'-50px'}, "slow")
-    },1200);
-    setTimeout(function () {
-        $("img.img-circle").animate({height:'auto',width: '10%'}, "slow")
-    },1200);
-    setTimeout(function () {
-        $(".social").fadeIn(1200);
-    },2000);
-}
+});
