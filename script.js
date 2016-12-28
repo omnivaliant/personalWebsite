b24950cb70bbf50600d9eb8a6f7aa0ef381d3050
Mohit Patel <omnivaliant@gmail.com>
2016-12-28 00:17:47 -0500

    Changed icon animations and positions.

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
 $(function() {
-
-    $(".fa-linkedin-square").mouseover(function(){
-        $(this).css("color","red");
-    });
-    $(".fa-github").mouseover(function(){
-        $(this).css("color","red");
-    });
     
 });
