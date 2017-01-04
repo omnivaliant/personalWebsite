ae7f20de7340f65275223ec918f9f649771dc8b2
Mohit Patel <omnivaliant@gmail.com>
2017-01-04 00:06:05 -0500

    Changed colour scheme and front page design.

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
-$(function() {
-    
+$(document).ready(function () {
+
+    // Smooth scrolling when clicking navbar to move between sections
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 750, function () {
+
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    //Change active navbar icons depending on where the page is scrolled
+
+    var aChildren = $("nav li").children();
+    var aArray = [];
+    for (var i = 0; i < aChildren.length; i++) {
+        var aChild = aChildren[i];
+        var ahref = $(aChild).attr('href');
+        aArray.push(ahref);
+    }
+
+    $(window).scroll(function () {
+        var windowPos = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        var docHeight = $(document).height();
+
+        for (var i = 0; i < aArray.length; i++) {
+            var theID = aArray[i];
+            var divPos = $(theID).offset().top;
+            var divHeight = $(theID).height();
+            if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+                $("a[href='" + theID + "']").addClass("nav-active");
+            } else {
+                $("a[href='" + theID + "']").removeClass("nav-active");
+            }
+        }
+
+        if (windowPos + windowHeight == docHeight) {
+            if (!$("nav li:last-child a").hasClass("nav-active")) {
+                var navActiveCurrent = $(".nav-active").attr("href");
+                $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
+                $("nav li:last-child a").addClass("nav-active");
+            }
+        }
+    });
 });
